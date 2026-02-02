6f5e7dffabd2675825661a634969826d249530a3
Ivan Todosiuk <ivan.todosiks@gmail.com>
2026-02-02 12:35:25 +0100

    Refactor skill levels in index.html and update contact form action. Removed db.php and send.php files for database connection and form handling.

---
+++ Db/submit.php
@@ -0,0 +1,32 @@
+<?php
+// Настройки подключения
+$servername = "172.16.1.98";
+$username = "portfolioivanimkattano"; // ваш пользователь MySQL
+$password = "QwerZxc924015";     // пароль пользователя MySQL
+$dbname = "db_portfolioivanimkattano";
+
+// Создаём подключение
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверяем подключение
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Получаем данные из формы и экранируем их
+$name = $conn->real_escape_string($_POST['name']);
+$email = $conn->real_escape_string($_POST['email']);
+$message = $conn->real_escape_string($_POST['message']);
+
+// SQL-запрос для вставки данных
+$sql = "INSERT INTO contacts (name, email, message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Данные успешно отправлены!";
+} else {
+    echo "Ошибка: " . $sql . "<br>" . $conn->error;
+}
+
+// Закрываем подключение
+$conn->close();
+?>
